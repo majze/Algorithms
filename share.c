e4862e196a1ce318dcac9486e092c64c78bf03a8
Donald <22397029+majze@users.noreply.github.com>
2020-01-23 14:21:20 -0500

    Update share.c

--- share.c
+++ share.c
@@ -8,7 +8,8 @@
 int readNum();
 void writeNum(int x);
 
-int main() {
+int main()
+{
 	int x = 19530;
 	char number[8];
 	pid_t piddly;
@@ -20,75 +21,67 @@ int main() {
 	fclose(create);
 	printf("Value of x = %d\n", x);
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5; i++)
+	{
 
 		printf("ITERATION: %d\n", i);
 		piddly = fork();
 
-		if (piddly == 0) { //child
-
+		if (piddly == 0) //child
+		{
 			x = readNum();
 			x -= 5;
 			printf("Child: x = %d\n", x);
 			writeNum(x);
 			exit(42);
-
 		}
-		else if (piddly > 0) { //parent
-
+		else if (piddly > 0) //parent
+		{
 			pid_t wait = waitpid(piddly, NULL, 0); //wait for child to finish
-			if (wait < 0) {
+			if (wait < 0)
+			{
 				printf("Error, child process did not finish");
 				return 3;
-
 			}
-			else {
-
+			else
+			{
 				x = readNum();
 				x /= 5;
 				printf("Parent: x = %d\n", x);
 				writeNum(x);
-
 			}
-
 		}
-		else {
-
+		else
+		{
 			printf("Fork call unsucessful");
 			return 4;
-
 		}
-
 		printf("\n");
-
 	}
 }
 
-int readNum() {
-
+int readNum()
+{
 	char number[8];
 	int result;
 	FILE *stream;
 	stream = fopen("num.txt", "r");
-	if (stream == NULL) {
-
+	if (stream == NULL)
+	{
 		perror("Cannot open file");
 		return (-1);
-
 	}
 	result = atoi(fgets(number, 8, stream));
 	fclose(stream);
 	return result;
-
 }
 
-void writeNum(int x) {
-
+void writeNum(int x)
+{
 	char number[8];
 	sprintf(number, "%d", x);
 	FILE *stream;
 	stream = fopen("num.txt", "w");
 	fputs(number, stream);
 	fclose(stream);
-
 }
